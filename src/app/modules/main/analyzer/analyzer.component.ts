41da304eb91fad5ddd24cdba737c4932464d4a19
patrickvrb <patrickvrb@gmail.com>
2021-06-09 00:39:24 -0300

    Visual adjustments and script data fetch

--- src/app/modules/main/analyzer/analyzer.component.ts
+++ src/app/modules/main/analyzer/analyzer.component.ts
@@ -1,54 +1,58 @@
-import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
-import AgentFacade from 'src/app/core/facades/agent.facade';
-import CallFacade from 'src/app/core/facades/call.facade';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from "@angular/core";
+import { MatSlider } from "@angular/material/slider";
+import { Router } from "@angular/router";
+import AgentFacade from "src/app/core/facades/agent.facade";
+import CallFacade from "src/app/core/facades/call.facade";
 
-import TemplateService from 'src/app/core/services/template.service';
+import TemplateService from "src/app/core/services/template.service";
 
 @Component({
-  selector:        'app-analyzer',
-  templateUrl:     './analyzer.component.html',
-  styleUrls:       ['./analyzer.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-analyzer",
+  templateUrl: "./analyzer.component.html",
+  styleUrls: ["./analyzer.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AnalyzerComponent implements OnInit, AfterViewInit {
-  @ViewChild('subHeader')
+  @ViewChild("subHeader")
   private subHeader?: TemplateRef<any>;
-  public dataSource: any[]    = [];
+  public dataSource: any[] = [];
   public dataSourceRep: any[] = [];
-
+  value = 1;
   constructor(
     public agents: AgentFacade,
     public calls: CallFacade,
     private _tplService: TemplateService,
     private _router: Router
-  ) {
-  }
+  ) {}
 
   public ngAfterViewInit(): void {
-    this._tplService.register('subHeader', this.subHeader);
+    this._tplService.register("subHeader", this.subHeader);
   }
 
   public ngOnInit(): void {
-    this.dataSource    = MOCK_DATA();
+    this.dataSource = MOCK_DATA();
     this.dataSourceRep = MOCK_DATA().slice(-25);
   }
 
   public selectAgent(event: any): void {
-    this.agents.setActiveAgent(event.target?.value);
-  };
+    this.agents.setActiveAgent(event.value);
+  }
 
   public selectCall(event: any): void {
-    this.calls.selectCall(event.target?.value);
+    this.calls.selectCall(event.value);
   }
 }
 
 const MOCK_DATA = () => {
-  const DATA: any[]        = [];
-  const SPEAKERS: string[] = [
-    'Harvey',
-    'Luke'
-  ];
+  const DATA: any[] = [];
+  const SPEAKERS: string[] = ["Harvey", "Luke"];
 
   let currentTime = 30;
 
@@ -57,22 +61,12 @@ const MOCK_DATA = () => {
     const sec = Math.floor(currentTime - min * 60);
 
     DATA.push({
-      time:     `${(
-        '0' + min
-      ).slice(-2)}:${(
-        '0' + sec
-      ).slice(-2)}`,
-      speaker:  SPEAKERS[Math.floor(Math.random() *
-        (
-          SPEAKERS.length
-        ))],
-      sentence: `This is a sample sentence #${i + 1}`
+      time: `${("0" + min).slice(-2)}:${("0" + sec).slice(-2)}`,
+      speaker: SPEAKERS[Math.floor(Math.random() * SPEAKERS.length)],
+      sentence: `This is a sample sentence #${i + 1}`,
     });
 
-    currentTime +=
-      (
-        Math.random() * 10
-      ) + 5;
+    currentTime += Math.random() * 10 + 5;
   }
 
   return DATA;
--- src/app/modules/main/analyzer/analyzer.module.ts
+++ src/app/modules/main/analyzer/analyzer.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { CoreModule } from 'src/app/core/core.module';
+import { CoreModule } from "src/app/core/core.module";
+import { MaterialModule } from "../../material/material.module";
 
-import AnalyzerComponent from './analyzer.component';
-import { ROUTES } from './analyzer.routes';
+import AnalyzerComponent from "./analyzer.component";
+import { ROUTES } from "./analyzer.routes";
 
 @NgModule({
-    declarations: [ AnalyzerComponent ],
-    imports: [
-        CoreModule,
-        RouterModule.forChild(ROUTES),
-        ReactiveFormsModule
-    ]
+  declarations: [AnalyzerComponent],
+  imports: [
+    CoreModule,
+    FormsModule,
+    RouterModule.forChild(ROUTES),
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
 })
 export class AnalyzerModule {}
